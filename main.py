f9548d117f933d8456c2ab23466db6b289327578
Sharath <sharathwelcomesyouin@gmail.com>
2022-06-25 16:48:50 +0530

    add count_down timer function to start button

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
-
+def click_on_start():
+    count_down(5)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
     if count>= 0:
@@ -41,12 +42,11 @@ canvas.create_image(100,112,image=tomato_img)
 timer_text=canvas.create_text(100,130, text="00:00", fill="white", font=(FONT_NAME,22,"bold"))
 # canvas.pack()
 canvas.grid(row=1,column=1)
-count_down(5)
 
 timer_label=Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME,22,"bold"))
 timer_label.grid(row=0,column=1)
 
-start_button=Button(text="Start")
+start_button=Button(text="Start", command=click_on_start)
 start_button.grid(row=2, column=0)
 
 reset_button=Button(text="Reset")
