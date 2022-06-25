cc20da92529fa9e35360d23a5ddde34470063f16
Sharath <sharathwelcomesyouin@gmail.com>
2022-06-25 16:32:26 +0530

    modify event Listener for my_window

--- main.py
+++ main.py
@@ -21,10 +21,12 @@ LONG_BREAK_MIN = 20
 my_window=Tk()
 my_window.title("PomoDoro")
 
-def do_something(thing):
+def do_something(thing, something, another_thing):
     print(thing)
+    print(something)
+    print(another_thing)
 
-my_window.after(1000, do_something, "Hello world")
+my_window.after(1000, do_something, "Hello world", "test",456)
 
 my_window.config(padx=100, pady=50, bg=YELLOW)
 canvas=Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
